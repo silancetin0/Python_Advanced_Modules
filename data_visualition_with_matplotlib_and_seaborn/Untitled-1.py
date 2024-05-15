cdac102df1138004b351af8fa5b5d0757986b361
silancetin0 <silancetin8@gmail.com>
2024-05-15 17:24:42 +0300

    Python Advanced Modules-Data Visualition

---
+++ data_visualition_with_matplotlib_and_seaborn/Untitled-1.py
@@ -0,0 +1,16 @@
+musterıAdı= "ali"
+musteriSoyadı= "cet"
+musterıAdSoyad= musterıAdı+ ' '+ musteriSoyadı
+musterıCinsiyet= True
+musterıTcKimlik='12304569885'
+musterıDogumYılı =''
+
+print("ad gir")
+name = input()
+print("merhaba"+ name)
+
+x=input()
+
+print(type(x))
+
+result= 'www.silancetin8@gmail.com'
